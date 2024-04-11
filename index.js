4c1fd81eafc82327025ce5b8de12fe81c8332e28
Upbolt <nnrv.ous@gmail.com>
2024-04-11 02:19:00 -0700

    change main file to native module

--- index.js
+++
@@ -1,8 +0,0 @@
-const {
-	disassemble,
-	disassembleBytecode,
-} = require("./build/Release/simple_lua_disassembler");
-
-// console.log([process.platform, process.arch, process.versions.modules]);
-
-console.log(`${disassemble("print'hi'")}`);
